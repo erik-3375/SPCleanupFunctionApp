9b6c92ae5139f3b451e4cc3d00a0c7c9cdb28aa3
Erik Johnson <erikj2388@gmail.com>
2025-08-01 10:55:26 -0400

    Created the remaining Video functions. Adjusted schedule to run 9-5 every day.

---
+++ VideoOTS-Cleanup-05-May-2025/run.ps1
@@ -0,0 +1,25 @@
+param($Timer)
+
+# Define function-specific value
+Import-Module CleanupTools
+
+$tenantId = "7318a427-2f81-408f-8386-6569e958a870"
+$clientId = "d01ba7da-01d7-4b6d-8bb4-fab5d10e0e24"
+$clientSecret = $env:GraphClientSecret
+
+$hostname = "planetdepos.sharepoint.com"
+$sitePath = "sites/VideoJobSubmissionPortal"
+$libraryName = "Jobs"
+$libraryPath = "Original Tech Submissions/2025/05 May"
+$folderAgeThresholdDays = 60
+
+# Call shared logic
+Invoke-FolderCleanup `
+    -tenantId $tenantId `
+    -clientId $clientId `
+    -clientSecret $clientSecret `
+    -hostname $hostname `
+    -sitePath $sitePath `
+    -libraryName $libraryName `
+    -libraryPath $libraryPath `
+    -folderAgeThresholdDays $folderAgeThresholdDays
