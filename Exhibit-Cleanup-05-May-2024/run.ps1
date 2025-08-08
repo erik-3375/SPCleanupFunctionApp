843bdedaee080f42af2803543597c8958cae4cb3
Erik Johnson <erikj2388@gmail.com>
2025-08-08 11:37:35 -0400

    Switched Digitals and Exhibits over to using the new Module and adjusted schedule to run hourly 9-5 Mon-Fri

--- Exhibit-Cleanup-05-May-2024/run.ps1
+++ Exhibit-Cleanup-05-May-2024/run.ps1
@@ -1,7 +1,7 @@
 param($Timer)
 
 # Define function-specific value
-Import-Module CleanupTools
+Import-Module FolderCleanup
 
 $tenantId = "7318a427-2f81-408f-8386-6569e958a870"
 $clientId = "d01ba7da-01d7-4b6d-8bb4-fab5d10e0e24"
